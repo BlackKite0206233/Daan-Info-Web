be19049e65a347fa956d01989f945549626902fb
BlackKite0206233 <samuel991640@gmail.com>
2016-01-28 23:13:04 +0800

    classController內部程式碼

--- app/Http/Controllers/classController.php
+++ app/Http/Controllers/classController.php
@@ -7,25 +7,38 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
+use daan_info_web\Repositories\classRepositories;
+
 class classController extends Controller
 {
+    protected $classRepositories;
+
+    public function __construct(classRepositories $classRepositories)
+    {
+        $this->classRepositories = $classRepositories;
+    }
     //
     public function store(Request $request)//post class
     {
         //新增分類
-
+        if(isset($request['typename']))
+            $this->classRepositories
+                ->insert($request['typename']);
     }
 
     public function update(Request $request)//put class/{class}
     {
         //編輯分類
-
+        if(isset($request['typename']))
+            $this->classRepositories
+                ->edit($request['id'],$request['typename']);
     }
 
     public function destroy(Request $request)// delete class/{class}
     {
         //刪除分類
-
+        $this->classRepositories
+            ->delete($request['id']);
     }
 
     public function index()// get class/
