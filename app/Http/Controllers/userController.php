8949b10a70f526dd7280fe3b84cf3657dd91654b
BlackKite0206233 <samuel991640@gmail.com>
2016-01-29 10:42:40 +0800

    userController內部程式碼

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -9,9 +9,16 @@ use daan_info_web\Repositories\UserRepository;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use daan_info_web\Repositories\userRepositories;
 
 class userController extends Controller
 {
+    protected $userRepositories;
+
+    public function __construct(userRepositories $userRepositories)
+    {
+        $this->userRepositories = $userRepositories;
+    }
     //
     public function index()// get /login
     {
@@ -21,12 +28,15 @@ class userController extends Controller
     public function create(Request $request)// post /login
     {
         //登入驗證
-
+        $this->userRepositories
+            ->login($request['acc'],$request['password']);
     }
 
     public function logout()
     {
         //登出
+        $this->userRepositories
+            ->logout();
 
     }
 
